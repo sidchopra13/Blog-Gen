b4c1e4bd49e1c13fc65800430f152b1a0545cc46
sidchopra13 <62833062+sidchopra13@users.noreply.github.com>
2022-07-01 00:58:47 +0530

    commit before hosting on heroku.

---
+++ app.js
@@ -0,0 +1,84 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const _ = require("lodash");
+const mongoose = require('mongoose');
+
+const homeStartingContent = "Welcome to Blog Gen! Blog Gen is a dynamic blogging web application where you can publish your blogs with just a click! This website is made with Node.js and EJS, which is a template engine.";
+const aboutContent = "Welcome to Blog Gen! Blog Gen is a dynamic blogging web application where you can publish your blogs with just a click! This website is made with Node.js and EJS, which is a template engine.";
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/blogDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+  }
+);
+
+const postSchema = {
+  title: String,
+  content: String,
+  createdAt: { type: Date, default: Date.now }
+};
+
+const Post = mongoose.model("Post", postSchema);
+
+app.get("/", function(req, res){
+
+  Post.find({}, function(err, posts){
+    res.render("home", {
+      startingContent: homeStartingContent,
+      posts: posts
+      });
+  });
+});
+
+app.get("/compose", function(req, res){
+  res.render("compose");
+});
+
+app.post("/compose", function(req, res){
+  const post = new Post({
+    title: req.body.postTitle,
+    content: req.body.postBody
+  });
+
+  post.save(function(err){
+    if (!err){
+        res.redirect("/");
+    }
+  });
+});
+
+app.get("/posts/:postId", function(req, res){
+
+const requestedPostId = req.params.postId;
+
+  Post.findOne({_id: requestedPostId}, function(err, post){
+    res.render("post", {
+      title: post.title,
+      content: post.content
+    });
+  });
+
+});
+
+app.get("/about", function(req, res){
+  res.render("about", {aboutContent: aboutContent});
+});
+
+app.get("/contact", function(req, res){
+  res.render("contact");
+});
+
+
+app.listen(process.env.PORT||3000, function() {
+  console.log("Server started on port 3000");
+});
